efbd8a6a51060c0a8581d1e928b184dbf4bdf5fe
ogt6590 <ogt6590@gmail.com>
2023-01-06 18:30:01 +0900

    7번 state 까지 완료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
+function Article(props) {
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
+}
 
 function Header(props) {
-  console.log('props', props.title);
   return <header>
-    <h1><a href="/">{props.title}</a></h1>
+    <h1><a href="/" onClick={(event)=>{
+      event.preventDefault();
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
   </header>
 }
 
@@ -13,7 +24,12 @@ function Nav(props) {
 
   for(let i=0; i<props.topics.length; i++){
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>);
+    lis.push(<li key={t.id}>
+        <a id={t.id} href={'/read/'+t.id} onClick={event=>{
+          event.preventDefault();
+          props.onChangeMode(Number(event.target.id));
+        }}>{t.title}</a>
+      </li>);
   }
 
   return <nav>
@@ -23,24 +39,56 @@ function Nav(props) {
   </nav>
 }
 
-function Article(props) {
+function Create(){
   return <article>
-    <h2>{props.title}</h2>
-    {props.body}
+    <h2>Create</h2>
+    <form>
+      <p><input type="text" name="title" placeholder="title"></input></p>
+      <p><textarea name="body" placeholder="body"></textarea></p>
+    </form>
   </article>
 }
 
+
 function App() {
+  const [mode,setMode] = useState('WELCOME');
+  const [id,setId] = useState(null);
   const topics = [
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javascript', body:'javascript is ...'}
   ]
+
+  let content = null;
+  if(mode === 'WELCOME'){
+    content =  <Article title="Welcome" body="Hello, WEB"></Article>
+  } else if(mode === 'READ'){
+    let title, body = null;
+    for(let i=0; i<topics.length; i++){
+      if(topics[i].id === id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content =  <Article title={title} body={body}></Article>
+  } else if(mode === 'CREATE'){
+    content = <Create></Create>
+  }
+
   return (
     <div>
-      <Header title="WEB"></Header>
-      <Nav topics={topics}></Nav>
-      <Article title="Welcome" body="Hello, WEB"></Article>
+      <Header title="WEB" onChangeMode={()=>{
+       setMode('WELCOME');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(_id)=>{
+        setMode('READ');
+        setId(_id);
+      }}></Nav>
+      {content}
+      <a href="/create" onClick={event=>{
+        event.preventDefault();
+        setMode('CREATE');
+      }}>Create</a>
     </div>
   );
 }
